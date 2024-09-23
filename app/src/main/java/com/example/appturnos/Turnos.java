3cae678deb87d48779a808e29a146532451adf57
Tomas <54193905+TomasPisarski@users.noreply.github.com>
2024-09-23 20:03:41 -0300

    chore: colores

--- app/src/main/java/com/example/appturnos/Turnos.java
+++ app/src/main/java/com/example/appturnos/Turnos.java
@@ -1,6 +1,7 @@
 package com.example.appturnos;
 
 import android.graphics.Typeface;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -22,7 +23,7 @@ public class Turnos extends AppCompatActivity {
         LinearLayout container = findViewById(R.id.linearTurnos);
 
         List<Turno> turnos = Arrays.asList(
-                new Turno("Tomás Sosa - 41684756 #594525484", "8:00 AM - 9:00 AM", "Dirección 1"),
+                new Turno("Tomás Sosa - 41684756", "8:00 AM - 9:00 AM", "Dirección 1"),
                 new Turno("Turno 2", "9:00 AM - 10:00 AM", "Dirección 2"),
                 new Turno("Turno 3", "10:00 AM - 11:00 AM", "Dirección 3"),
                 new Turno("Turno 4", "10:00 AM - 11:00 AM", "Dirección 4"),
@@ -41,23 +42,33 @@ public class Turnos extends AppCompatActivity {
             LinearLayout turnoLayout = new LinearLayout(this);
             turnoLayout.setOrientation(LinearLayout.VERTICAL);
             turnoLayout.setBackgroundColor(ContextCompat.getColor(this, R.color.turno_background));
+            turnoLayout.setPadding(20, 30, 20, 30);
+
+            // Crea un GradientDrawable
+            GradientDrawable drawable = new GradientDrawable();
+            drawable.setShape(GradientDrawable.RECTANGLE);
+            drawable.setColor(ContextCompat.getColor(this, R.color.turno_background)); // Establece el color de fondo
+            drawable.setCornerRadius(20f); // Establece el radio de las esquinas (20dp en este caso)
+            drawable.setStroke(2, ContextCompat.getColor(this, R.color.black)); // Establece el borde (2px de grosor, color negro)
+
+            turnoLayout.setBackground(drawable);
 
             TextView titulo = new TextView(this);
             titulo.setText(turno.getTitulo());
             titulo.setTextSize(18f);
-            titulo.setTextColor(ContextCompat.getColor(this, R.color.black));
+            titulo.setTextColor(ContextCompat.getColor(this, R.color.white));
             titulo.setTypeface(null, Typeface.BOLD);
 
             TextView subtitulo = new TextView(this);
             subtitulo.setText(turno.getHorario());
             subtitulo.setTextSize(16f);
-            subtitulo.setTextColor(ContextCompat.getColor(this, R.color.black));
+            subtitulo.setTextColor(ContextCompat.getColor(this, R.color.white));
 
 
             TextView body = new TextView(this);
             body.setText(turno.getDireccion());
             body.setTextSize(14f);
-            body.setTextColor(ContextCompat.getColor(this, R.color.black));
+            body.setTextColor(ContextCompat.getColor(this, R.color.white));
 
 
             turnoLayout.addView(titulo);
@@ -66,7 +77,7 @@ public class Turnos extends AppCompatActivity {
 
             LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
-            layoutParams.setMargins(40, 0, 0, 0);
+            layoutParams.setMargins(50, 10, 50, 0);
             turnoLayout.setLayoutParams(layoutParams);
 
             container.addView(turnoLayout);
